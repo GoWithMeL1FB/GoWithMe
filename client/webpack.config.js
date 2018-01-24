14400b5905c7aa5014db1216ac122b0ff9a968c6
l1fb <yon_gospel@hotmail.com>
2018-01-23 20:04:44 -0800

    [front-end] implements working webpack with watch and basic app.jsx and index.jsx

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
   output: {
     filename: './public/bundle.js'
   },
-  watch: true
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+        }
+      }
+    ]
+  }
 }
