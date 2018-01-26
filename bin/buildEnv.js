368ea0a4dfeb79290e4e5c36cfc59ad85c790ffd
Kevin Vo <kevinvoduy218@gmail.com>
2018-01-26 13:18:59 -0800

    [env] - builds env files

--- bin/buildEnv.js
+++ bin/buildEnv.js
@@ -1,1 +1,20 @@
 //build env
+const fs = require('fs');
+const _ = require('lodash');
+
+const envVariables = require('../config/.env.sample');
+
+const createENVFile = (directory, variables) => {
+  _.each(variables, (variable) => {
+    fs.appendFileSync(`./${directory}/.env`, variable + '\n');
+  })
+}
+
+const buildEnv = () => {
+  _.each(envVariables, (value, key) => {
+    fs.writeFileSync(`./${key}/.env`, '')
+    createENVFile(key, value);
+  });
+}
+
+buildEnv();
