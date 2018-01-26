6f498ab49ce15e3eb04c83d6fcbb6b278a71f97f
Kevin Vo <kevinvoduy218@gmail.com>
2018-01-26 13:12:44 -0800

    [env] - sample port setup

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const server = express();
-server.use(bodyParser.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3050;
 
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, '../client/public')));
+
 server.use('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/public/index.html'));
-})
+}) // at the moment, server is using / listening on all ports
 
-server.listen(3050, () => console.log('static files are being served'));
+server.listen(PORT, () => console.log('static files are being served'));
