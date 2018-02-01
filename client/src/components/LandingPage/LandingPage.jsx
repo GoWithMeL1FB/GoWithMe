ee6c6fd0b99d77e787dfc9d9ea64e90b76f9f082
David Yonwook Chung <yon_gospel@hotmail.com>
2018-02-01 14:25:14 -0800

    [front] adds two buttons on the landing page.

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './LandingPage.scss';
 import Parallax from 'react-materialize';
+import { Button } from 'react-materialize';
 
 class LandingPage extends Component {
   render() {
     return (
-      <div class='landingpage'>
-       <div id='logo'>&#9736;</div>
-       <h2>GO</h2>
-       <h2>WITH</h2>
-       <h2>ME</h2>
-       <h3>we plan. you play.</h3>
+      <div className='landingpage'>
+        
+        <Button
+          waves='light'
+          onClick={() => console.log('hi')}
+          text='sign up'
+        ></Button>
+
+        <Button
+          waves='light'
+          onClick={() => console.log('hi')}
+          text='log in'
+        ></Button>
+        
+        <div id='logo'>&#9736;</div>
+        
+        <h2>GO</h2>
+        <h2>WITH</h2>
+        <h2>ME</h2>
+        <h3>we plan. you play.</h3>
       </div>
+
     )
   }
 }
