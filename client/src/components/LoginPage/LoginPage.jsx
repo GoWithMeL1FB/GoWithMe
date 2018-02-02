02fd637b1843f2044322d74e727b38b45f117af4
Karolee <kcarlson957@gmail.com>
2018-02-01 16:00:27 -0800

    fixed eslint

--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
       <div className="container">
-      <p>Login</p>
-      <div className="row">
-      <div className="col-25">
-       
-
-      </div>
-      <div className="col-75">
-        <input type="text" id="fname" nameName="firstname" placeholder="Your Username"/>
-        <br/>
-        <input type="text" id="fname" nameName="firstname" placeholder="Your password"/>
-      </div>
-    </div>
-    <a><button> Submit </button></a>
-      </div>
-    )
+        <p>Login</p>
+        <div className="row">
+          <div className="col-25" />
+          <div className="col-75">
+            <input
+              type="text"
+              id="fname"
+              nameName="firstname"
+              placeholder="Your Username"
+            />
+            <input
+              type="text"
+              id="fname"
+              nameName="firstname"
+              placeholder="Your password"
+            />
+          </div>
+        </div>
+        <button> Submit </button>
+        </div>
+    );
   }
 }
 
