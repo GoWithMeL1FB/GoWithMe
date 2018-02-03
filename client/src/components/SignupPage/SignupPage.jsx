6c3b04ab9ec48d85399381e0e38d0042af875f11
David Yonwook Chung <yon_gospel@hotmail.com>
2018-02-03 15:36:39 -0800

    [front] implements basic structure of signup and login functionality

--- client/src/components/SignupPage/SignupPage.jsx
+++ client/src/components/SignupPage/SignupPage.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import { Row, Input, Button, Icon, Modal } from 'react-materialize';
+import axios from 'axios';
 
 class SignupPage extends Component {
+
+  createUser() {
+    axios.post('/signup', {
+      function(){
+        console.log('sign up button reacting')
+      }
+    })
+  }
 
   render() {
     return (
@@ -10,11 +19,14 @@ class SignupPage extends Component {
         trigger={<Button waves='light'> Signup <Icon left>fiber_new</Icon></Button>}
         >
         <Row>
-		      <Input placeholder="Placeholder" s={6} label="First Name" />
+          <Input s={6} label="Username" />
+          <Input s={6} label="BirthYear" />
+		      <Input s={6} label="First Name" />
 		      <Input s={6} label="Last Name" />
-	    	  <Input type="password" label="password" s={12} />
+	    	  <Input type="password" label="Password" s={12} />
     		  <Input type="email" label="Email" s={12} />
         </Row>
+        <Button>Submit</Button>
       </Modal>
     )
   }
