08f23cd49cfff8e4c6698885d1c2bcdc30b47ac1
Kevin Vo <kevinvoduy218@gmail.com>
2018-02-05 16:40:46 -0800

    [merge]

--- client/src/components/global/Footer/Footer.jsx
+++ client/src/components/global/Footer/Footer.jsx
@@ -2,27 +2,27 @@ import React, { Component } from 'react';
 import './Footer.scss';
 
 class Footer extends Component {
-  
+
   render() {
     return (
-        <footer class="page-footer blue">
-            <div class="container">
-                <div class="row">
-                    <div class="col l6 s12">
-                        <h5 class="white-text">go with me</h5>
-                        <p class="grey-text text-lighten-4"></p>
+        <footer className="page-footer blue">
+            <div className="container">
+                <div className="row">
+                    <div className="col l6 s12">
+                        <h5 className="white-text">go with me</h5>
+                        <p className="grey-text text-lighten-4"></p>
                     </div>
-                    <div class="col l4 offset-l2 s12">
-                        <h5 class="white-text">About gowithme</h5>
+                    <div className="col l4 offset-l2 s12">
+                        <h5 className="white-text">About gowithme</h5>
                         <ul>
-                            <li><a class="grey-text text-lighten-3" href="#!">About us</a></li>
-                            <li><a class="grey-text text-lighten-3" href="#!">Contact Us</a></li>
+                            <li><a className="grey-text text-lighten-3" href="#!">About us</a></li>
+                            <li><a className="grey-text text-lighten-3" href="#!">Contact Us</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div class="footer-copyright">
-                <div class="container">
+            <div className="footer-copyright">
+                <div className="container">
                     © 2018 Copyright gowithme Inc. All rights reserved.
                 </div>
             </div>
