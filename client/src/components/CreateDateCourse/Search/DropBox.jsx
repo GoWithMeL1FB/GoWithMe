e64661c05a61da1a13ecfb550298f675db5165d3
David Yonwook Chung <yon_gospel@hotmail.com>
2018-02-08 15:23:38 -0800

    [front] implements handling of the new event entries in the event entries boxes

--- client/src/components/CreateDateCourse/Search/DropBox.jsx
+++ client/src/components/CreateDateCourse/Search/DropBox.jsx
@@ -23,12 +23,33 @@ class DropBox extends React.Component {
     return(
       <Row>
         <Col s={12}>
+        {this.state.dateCourse.map(venue => {
+          // let price = venue.venue.price?venue.venue.price.message:null;
+          return(
+          <div key={venue.id}>
+          <DragDropContainer 
+            item={venue}
+            returnToBase={true}
+            dragData={{
+              venue: venue
+            }}
+            >
+            <Events 
+            id={venue.id}
+            name={venue.venue.name}
+            address={venue.venue.location.address}
+            // price={price}
+            category={venue.venue.categories[0].name}
+            prefix={venue.photo.prefix}
+            suffix={venue.photo.suffix}
+            />
+           </DragDropContainer>
+          </div>
+        )})}
+
         <DropTarget 
           dragData={{
-            venue: this.props.venue,
-            item: this.props.item,
-            label: this.props.children,
-            index: this.props.index
+            venue: this.props.venue
           }}
           onHit={this.handleDrop}
           >
