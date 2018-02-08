1db01b6b268d9ccbb625b635d0e33a1b0ddc4daa
Kevin Vo <kevinvoduy218@gmail.com>
2018-02-08 13:09:04 -0800

    [homepage] - tiles kinda set up / waiting for data to iterate thru

--- client/src/components/HomeView/HomeView.jsx
+++ client/src/components/HomeView/HomeView.jsx
@@ -5,16 +5,36 @@ import Featured from './featuredDateCourse/featuredDateCourse.jsx'
 import OtherDateCourse from './otherDateCourse/otherDateCourse.jsx';
 
 class HomeView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      events: [{title:'Event 1\'s Title', desc:'Descprition of event 1'},{title:'Event 2\'s Title', desc:'Desccription of event 2'}]
+    }
+    this.onClickHandler = this.onClickHandler.bind(this);
+  }
+
+  componentDidMount() {
+    // get all events
+  }
+
+  // onClick for getting more event information
+  onClickHandler() {
+    // do something...like show info? or re-router?
+  }
+
   render() {
     return (
       <div>
-        <Featured></Featured>
-        <OtherDateCourse></OtherDateCourse>
-        <OtherDateCourse></OtherDateCourse>
-        <OtherDateCourse></OtherDateCourse>
-        <OtherDateCourse></OtherDateCourse>
-        <OtherDateCourse></OtherDateCourse>
-        <OtherDateCourse></OtherDateCourse>
+        <Featured />
+        {
+          this.state.events.map((event, index) => (
+            <OtherDateCourse
+              event={event}
+              key={index}
+              onClick={this.onClickHandler}
+            />
+          ))
+        }
       </div>
     )
   }
