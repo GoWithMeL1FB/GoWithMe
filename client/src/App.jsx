de785d4a5b52b5ed468a3b63d4f8a1043af98d09
Karolee <kcarlson957@gmail.com>
2018-02-08 17:38:38 -0800

    moved private route function to global folder for seperate of concerns

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,11 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
 import { render } from 'react-dom';
+import { PrivateRoute } from './components/global/Private/Private.js';
 import LandingPage from './components/LandingPage/LandingPage.jsx';
 import Home from './components/Home/Home.jsx';
 import EditProfile from './components/EditProfile/EditProfile.jsx';
 import LoginPage from './components/LoginPage/LoginPage.jsx';
-import jwtDecode from 'jwt-decode'
-
-const PrivateRoute = ({component: Component, ...rest}) => {
-  const { exp } = jwtDecode(sessionStorage.getItem('authentication'))
-  return(
-    <Route  {...rest} render={props => (
-        
-        exp < Math.floor(Date.now() / 1000) ? (
-          <Component {...props}/>
-        ) : (
-          <Redirect to={{
-            pathname: '/Login',
-            state: {from: props.location}
-          }}/>
-        )
-      )}/>
-  )
-}
 
 class App extends Component {
   constructor(props) {
---
+++ client/src/components/global/Private/Private.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
+import { render } from 'react-dom';
+import jwtDecode from 'jwt-decode';
+
+
+export const PrivateRoute = ({component: Component, ...rest}) => {
+  const { exp } = jwtDecode(sessionStorage.getItem('authentication'))
+  return(
+    <Route  {...rest} render={props => (
+        
+        exp > Math.floor(Date.now() / 1000) ? (
+          <Component {...props}/>
+        ) : (
+          <Redirect to={{
+            pathname: '/Login',
+            state: {from: props.location}
+          }}/>
+        )
+      )}/>
+  )
+};
