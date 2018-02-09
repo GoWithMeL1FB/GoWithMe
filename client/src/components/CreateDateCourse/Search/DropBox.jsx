9e9692e5b09f780f62e92e6d0b1673e548a91948
David Yonwook Chung <yon_gospel@hotmail.com>
2018-02-08 17:34:34 -0800

    [front] Implements event entries box

--- client/src/components/CreateDateCourse/Search/DropBox.jsx
+++ client/src/components/CreateDateCourse/Search/DropBox.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import Events from '../../global/Events/Events.jsx';
 
-import { DropTarget } from 'react-drag-drop-container';
+import { DropTarget, DragDropContainer } from 'react-drag-drop-container';
 import { Row, Col, Icon } from 'react-materialize';
 
 class DropBox extends React.Component {
@@ -16,17 +16,79 @@ class DropBox extends React.Component {
 
   handleDrop = (e) => {
     this.state.dateCourse.push(e);
-    console.log(this.state);
+    console.log('state from handle drop', this.state)
+    this.setState({dateCourse: this.state.dateCourse});
+    this.eventEntries()
   }
+
+  eventEntries = () => {
+    // return (
+
+    //   `<div>
+    //   Hello
+    //   </div>`
+    console.log('venues=', this.state.dateCourse)
+    //   return this.state.dateCourse.map(venue => {
+    //     console.log('VENUE', venue)
+    //     // let price = venue.venue.price?venue.venue.price.message:null;
+    //     return `
+    //       <div key=${venue.id}>
+    //       <DragDropContainer 
+    //         item=${venue}
+    //         returnToBase=${true}
+    //         dragData=${{
+    //           venue: venue
+    //         }}
+    //         >
+    //         <Events 
+    //           id=${venue.id}
+    //           name=${venue.name}
+    //           address=${venue.location}
+
+    //           category=${venue.categories}
+             
+    //         />
+    //       </DragDropContainer>
+    //       </div>
+    //     `
+    // })[0]
+  }
+
+
+  // <div key=${venue.id}>
+  // <DragDropContainer 
+  //   item=${venue}
+  //   returnToBase=${true}
+  //   dragData=${{
+  //     venue: venue
+  //   }}
+  //   >
+  //   <Events 
+  //     id=${venue.id}
+  //     name=${venue.name}
+  //     address=${venue.location}
+  //     // price=${price}
+  //     category=${venue.categories}
+  //     // photo=${`${photo.prefix}36x36${photo.suffix}`}
+  //   />
+  // </DragDropContainer>
+  // </div>
 
   render () {
     return(
       <Row>
+        <li>
+          test
+          </li>
         <Col s={12}>
-        {this.state.dateCourse.map(venue => {
-          // let price = venue.venue.price?venue.venue.price.message:null;
-          return(
+        {
+        this.state.dateCourse.map((v) => {
+          let venue = v.dragData.venue.venue;
+          let pObj = v.dragData.venue;
+          
+          return (
           <div key={venue.id}>
+            <li>hello</li>
           <DragDropContainer 
             item={venue}
             returnToBase={true}
@@ -35,20 +97,21 @@ class DropBox extends React.Component {
             }}
             >
             <Events 
-            id={venue.id}
-            name={venue.venue.name}
-            address={venue.venue.location.address}
-            // price={price}
-            category={venue.venue.categories[0].name}
-            prefix={venue.photo.prefix}
-            suffix={venue.photo.suffix}
-            />
-           </DragDropContainer>
-          </div>
-        )})}
+              id={venue.id}
+              name={venue.name}
+              address={venue.location.address}
 
+              category={venue.categories[0].name}
+              prefix={pObj.photo.prefix}
+              suffix={pObj.photo.suffix}
+            />
+          </DragDropContainer>
+          </div>
+          )
+        })
+        }
         <DropTarget 
-          dragData={{
+          dropData={{
             venue: this.props.venue
           }}
           onHit={this.handleDrop}
