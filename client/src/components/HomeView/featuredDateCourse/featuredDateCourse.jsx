404c47562dd85fc4d9f8898de205262ef4848135
Kevin Vo <kevinvoduy218@gmail.com>
2018-02-10 02:09:20 -0800

    [add] - tech stack in footer

--- client/src/components/HomeView/featuredDateCourse/featuredDateCourse.jsx
+++ client/src/components/HomeView/featuredDateCourse/featuredDateCourse.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { Card, Col, CardTitle } from 'react-materialize';
+import { Card, Col, CardTitle, Badge } from 'react-materialize';
 
 class featuredDateCourse extends Component {
   render() {
     return (
       <Card className='small'
 	      header={<CardTitle image='http://images.dailyhive.com/20160602153203/Vancouver-Shutterstock.jpg'>Vancouver</CardTitle>}
-	      actions={[<a href='#'>Check out Date Course</a>]}>
-      	Come check out the beautiful city of Vancouver
+	      actions={[<a href='#'>Check out Date Course</a>,
+        <Badge className="new badge blue" data-badge-caption="Featured"/>]}>
+        Come check out the beautiful city of Vancouver
       </Card>
     )
   }
