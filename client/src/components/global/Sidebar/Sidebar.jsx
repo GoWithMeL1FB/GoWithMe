6b9342d7171e89e387eaada0a82b4ca7ddbc74f3
Karolee <kcarlson957@gmail.com>
2018-02-12 17:58:53 -0800

    updating page with user data.

--- client/src/components/global/Sidebar/Sidebar.jsx
+++ client/src/components/global/Sidebar/Sidebar.jsx
@@ -4,6 +4,36 @@ import { Row, Col } from 'react-materialize';
 import './Sidebar.css'
 class Sidebar extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: null,
+      email: null,
+      username: null,
+      birthday: null,
+      bio: null
+    }
+  }
+
+  async componentWillMount() {
+    await this.setState( {id: sessionStorage.getItem('id')})
+    console.log("Id is now", this.state.id)
+    axios.get('http://localhost:3030/api/user/fetchUsersInfo/' + this.state.id)
+      .then( result => {
+      console.log('this is the result', result.data[0]);
+      this.setState({
+        email: result.data[0].email,
+        username: result.data[0].username,
+        birthday: result.data[0].birthday,
+        bio: result.data[0].bio
+      })
+    })
+    .catch((err) => {
+      console.log('Log in Failed err:', err);
+    })
+
+  }
+
   render() {
     return (
       <div className="User">
@@ -13,9 +43,9 @@ class Sidebar extends Component {
         <img src="assets/images/prof.jpg"/>
         </div>
        
-        <p>David Chung</p>
-        <p>david@david.com</p>
-          
+        <p>{this.state.username}</p>
+        <p>{this.state.email}</p>
+        <p>{this.state.bio}</p>
         </Col>
       </Row>
   
