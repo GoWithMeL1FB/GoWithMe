d5402612aac21c6b7554d577f9e995fe13a2d594
Karolee <kcarlson957@gmail.com>
2018-02-13 11:35:44 -0800

    added input for imagelink.

--- client/src/components/EditProfile/EditProfile.jsx
+++ client/src/components/EditProfile/EditProfile.jsx
@@ -16,8 +16,8 @@ class EditProfile extends Component {
       email: 'kevin123@apple.com',
       bio: 'is tired most of the time',
       birthday: 1992,
-      dropImage: null
-  
+      profileimage: null,
+      
     }
     this.submitUpdate = this.submitUpdate.bind(this);
     this.onChangeHandler = this.onChangeHandler.bind(this);
@@ -57,24 +57,11 @@ class EditProfile extends Component {
       <div>
         <span>Edit Profile</span>
         <Row>
-        <Dropzone 
-                accept="image/jpeg, image/jpg, image/png"
-                multiple={false}
-                onDropAccepted={ this._onDrop.bind(this) } maxSize={ 2000000 }
-                onDragLeave= {this._onDrop.bind(this) } maxSize={ 2000000 }
-              >
-                <div>
-                  Click or drag photo here! Limit 2mb.
-                    {!this.state.imagePrev ? null : <div>Preview: <br/><img style={{maxHeight: '120px'}} src={this.state.imagePrev} /></div> }
-                </div>
-              </Dropzone>
-
-          </Row>
-        <Row>
           <Input s={6} name="firstname" label="First Name" onChange={this.onChangeHandler}/>
           <Input s={6} name="lastname" label="Last Name" onChange={this.onChangeHandler}/>
           <Input s={6} type="email" name="email" label="Email" onChange={this.onChangeHandler}/>
           <Input s={6} name="birthday" label="Birthday" onChange={this.onChangeHandler}/>
+          <Input s={6} name="profileImage" label="Image" onChange={this.onChangeHandler}/>
           <Input s={12} name="bio" label="Bio" onChange={this.onChangeHandler}/>
           <Button waves='light' onClick={this.submitUpdate}>submit</Button>
         </Row>
