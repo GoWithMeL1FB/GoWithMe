886521bab608eb7e1c135d32f6e2f0994360aee5
Kevin Vo <kevinvoduy218@gmail.com>
2018-02-13 20:36:47 -0800

    asdf

--- client/src/ReduxReducers/index.js
+++ client/src/ReduxReducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import LocationReducer from './LocationReducer.js';
 import SignUpReducer from './SignUpReducer';
 import dateCourseInfo from './CourseInfoReducer';
+import LoginReducer from './LoginReducer';
 //import an combine reducers here!!
 
 const allreducers = combineReducers({
   location: LocationReducer,
   signUpData: SignUpReducer,
-  dateCourseInfo: dateCourseInfo
+  dateCourseInfo: dateCourseInfo,
+  loginData: LoginReducer,
 });
 
 export default allreducers;
