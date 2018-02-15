523c3663b533ff924cd0da9d898120218ddbf92e
David Yonwook Chung <yon_gospel@hotmail.com>
2018-02-15 01:23:59 -0800

    [front] adds logo to the nav bar

--- client/src/components/global/Nav/Nav.jsx
+++ client/src/components/global/Nav/Nav.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { Navbar, NavItem, Icon } from 'react-materialize';
-import icon from "../../../../public/landingPageImage/pic/logos/gwmfavwhite.png";
-import iconTwo from "../../../../public/landingPageImage/pic/logos/gwmlogowhite.png"
+
 import './Nav.css';
+import logo from "../../../../public/landingPageImage/pic/logos/gwmfavwhite.png";
 
 class Nav extends Component {
 
@@ -12,17 +12,16 @@ class Nav extends Component {
       <div>
         <nav className="blue-grey darken-3" role="navigation">
           <div class="nav-wrapper container">
-            <a id="logo-container" href="#" class="brand-logo"><img id="airplane" src={icon}></img></a>
+            <a id="logo-container" href="#" class="brand-logo"><img id='brandlogo' src={logo}></img></a>            
             <ul class="right">
               <li><Link to="/Home/HomeView"><Icon>home</Icon></Link></li>
               <li><Link to="/Home/createDateCourse"><Icon>event_note</Icon></Link></li>
               <li><Link to="/Home/searchEvents"><Icon>search</Icon></Link></li>
-              <li><Link to="/Home/favorites"><Icon>turned_in</Icon></Link></li>
+              <li><Link to="/Home"><Icon>turned_in</Icon></Link></li>
               <li><Link to="/Home/editProfile"><Icon>account_circle</Icon></Link></li>
             </ul>
           </div>
         </nav>
-
       </div>
     )
   }
