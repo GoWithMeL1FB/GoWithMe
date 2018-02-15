724f541b8648756411e8a34cdc8d5a2077533efd
David Yonwook Chung <yon_gospel@hotmail.com>
2018-02-15 00:19:01 -0800

    [front] adds new logos and parallax image

--- client/src/components/global/Nav/Nav.jsx
+++ client/src/components/global/Nav/Nav.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { Navbar, NavItem, Icon } from 'react-materialize';
+import icon from "../../../../public/landingPageImage/pic/logos/gwmfavwhite.png";
+import iconTwo from "../../../../public/landingPageImage/pic/logos/gwmlogowhite.png"
+import './Nav.css';
 
 class Nav extends Component {
 
@@ -10,8 +13,7 @@ class Nav extends Component {
 
         <nav className="blue-grey darken-3" role="navigation">
           <div class="nav-wrapper container">
-            <a id="logo-container" href="#"></a>
-            <img src="../../../../public/landingPageImage/pic/logos/gwmfavwhite.png"></img>
+            <a id="logo-container" href="#" class="brand-logo"><img id="icon" src={icon}></img></a>
             <ul class="right">
               <li><Link to="/Home/HomeView"><Icon>home</Icon></Link></li>
               <li><Link to="/Home/createDateCourse"><Icon>event_note</Icon></Link></li>
