06d1fc0786bf8e16851dafb9eb5e557824b22b88
Kevin Vo <kevinvoduy218@gmail.com>
2018-02-16 01:23:43 -0800

    [favs] - properly display like count

---
+++ client/src/components/MyItin/eventONLY.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Collapsible, CollapsibleItem } from 'react-materialize';
+import url from '../../../config';
+
+
+class EventONLY extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+    };
+  }
+
+  async componentDidMount() {
+    console.log('props', this.props)
+    await axios.get(`${url.eventServer}/api/events/getEventById/${this.props.eventID.ID}`)
+      .then((res) => {
+        console.log('eventonly:', res);
+        this.setState({
+          events: res.data,
+        });
+      })
+  }
+
+  render() {
+    console.log('eventonly:', this.state);
+    return (
+      <CollapsibleItem header={(<span><strong>Event</strong>{' '}{this.state.events.name}</span>)} icon="assignment">
+        <strong>{this.state.events.description}</strong>{' '}
+        {this.state.events.location}
+      </CollapsibleItem>
+    )
+  }
+}
+
+  export default EventONLY;
