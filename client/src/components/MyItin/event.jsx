2f1e1b051ce3ac92a9fb04c405b67f202d1041a1
Kevin Vo <kevinvoduy218@gmail.com>
2018-02-15 18:00:56 -0800

    [faves] - shows itin data

--- client/src/components/MyItin/event.jsx
+++ client/src/components/MyItin/event.jsx
@@ -9,26 +9,43 @@ class Event extends Component {
     super(props);
     this.state = {
       events: [],
+      itinInfo: {},
     };
   }
 
   async componentDidMount() {
     await axios.get(`${url.eventServer}/api/events/getEventsByItin/5a854426bf93a36a17f50081`)
       .then((res) => {
-        console.log('res', res);
         this.setState({
           events: res.data,
         });
       })
+    await axios.get(`${url.eventServer}/api/itinerary/getItineraryById/5a854426bf93a36a17f50081`)
+      .then((res) => {
+        this.setState({
+          itinInfo: res.data
+        })
+      })
   }
 
   render() {
-    const { name, title, description } = this.state.events;
     console.log('state', this.state, this.props.itinID);
     return (
       <CollapsibleItem header={(<span><strong>Itinerary</strong></span>)} icon="assignment">
-        <strong>{name}</strong>
-        <span className="new badge" data-badge-caption="likes">6</span>
+        <strong>{this.state.itinInfo.title}</strong>
+        <span className="new badge" data-badge-caption="likes">56</span>
+        <Collapsible>
+        {
+          this.state.events.map((event, index) => {
+            return (
+              <CollapsibleItem header={event.name} key={index}>
+                <strong>{event.description}</strong>
+                <span className="new badge" data-badge-caption="likes">{event.meta.likes.length}</span>
+              </CollapsibleItem>
+            )
+          })
+        }
+        </Collapsible>
       </CollapsibleItem>
     )
   }
